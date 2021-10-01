779d2d956d4e2c9b5b75f38ff4f8e6a873e5db58
KazuwoKiwame12 <39262724+KazuwoKiwame12@users.noreply.github.com>
2021-10-01 16:19:04 +0900

    feat: find Gopher that is used and defined

--- find-gopher/main.go
+++ find-gopher/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"go/ast"
+	"go/importer"
 	"go/parser"
 	"go/token"
+	"go/types"
 	"log"
 )
 
@@ -15,7 +17,24 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	ast.Print(nil, f)
+	// Gopherという識別子が定義または利用されている部分を記録する
+	defsOrUses := map[*ast.Ident]types.Object{}
+	info := &types.Info{
+		Defs: defsOrUses,
+		Uses: defsOrUses,
+	}
+
+	// 型チェックを行うための設定
+	config := &types.Config{
+		Importer: importer.Default(),
+	}
+
+	// 型チェックを行う
+	_, err = config.Check("main", fset, []*ast.File{f}, info)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	ast.Inspect(f, func(n ast.Node) bool {
 		ident, ok := n.(*ast.Ident)
 		if !ok {
@@ -23,6 +42,16 @@ func main() {
 		}
 
 		if ident.Name != "Gopher" {
+			return true
+		}
+
+		obj := defsOrUses[ident]
+		if obj == nil {
+			return true
+		}
+
+		typ := obj.Type()
+		if _, ok := typ.(*types.Named); !ok {
 			return true
 		}
 
