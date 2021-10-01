2675d6a0050f90795fd088dc5cea7e02f7e1ad47
KazuwoKiwame12 <39262724+KazuwoKiwame12@users.noreply.github.com>
2021-10-01 15:36:31 +0900

    feat: search text, "Gopher"

--- find-gopher/main.go
+++ find-gopher/main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"go/ast"
 	"go/parser"
+	"go/token"
 	"log"
 )
 
 func main() {
-	expr, err := parser.ParseExpr("v + 1")
+	fset := token.NewFileSet()
+
+	f, err := parser.ParseFile(fset, "_gopher.go", nil, 0)
 	if err != nil {
 		log.Fatal(err)
 	}
-	ast.Print(nil, expr)
+	ast.Print(nil, f)
+	ast.Inspect(f, func(n ast.Node) bool {
+		ident, ok := n.(*ast.Ident)
+		if !ok {
+			return true
+		}
+
+		if ident.Name != "Gopher" {
+			return true
+		}
+
+		fmt.Println(fset.Position(ident.Pos()))
+		return true
+	})
 }
